d0a074622fc63b075cfb298587664c0ffad9f6bc
rhdzmota <rohdzmota@gmail.com>
2018-05-03 16:48:54 -0500

    add cli abstraction

--- main.py
+++ main.py
@@ -1,45 +1,13 @@
 import logging
 import json
-import os
 
-from optparse import OptionParser
 from conf.settings import FilesConfig, LogConf
 from ml.evaluate import procedure as eval_procedure
 from ml.search import procedure as random_procedure
+from util.cli import CLI
 
 RANDOM_SEARCH = "random-search"
 MODEL_EVALUATION = "model-eval"
-
-
-def check_options(opts):
-    logger.info("[function call] check_options(opts=%s)" % str(opts))
-    model_opt = getattr(opts, "model")
-    iter_opt = getattr(opts, "iter")
-    config_opt = getattr(opts, "config")
-    valid_models = ["xgboost", "random-forest"]
-
-    if model_opt is None:
-        raise ValueError("A model must be provided.")
-
-    if model_opt not in valid_models:
-        raise ValueError("Invalid model: %s not in %s" % (model_opt, str(valid_models)))
-
-    if iter_opt:
-        int(iter_opt)
-
-    if config_opt:
-        if not os.path.exists(config_opt):
-            raise ValueError("File %s does not exists." % config_opt)
-
-    if sum([isinstance(e, str) for e in [iter_opt, config_opt]]) != 1:
-        raise ValueError("Exactly one flag must be used: either --iter or --config")
-
-
-def determine_action(opts):
-    config_opt = getattr(opts, "config")
-    if not config_opt:
-        return RANDOM_SEARCH, getattr(opts, "iter")
-    return MODEL_EVALUATION, config_opt
 
 
 def main(model, param, action, submit):
@@ -62,18 +30,9 @@ def random_search(model, n, submit):
 
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("--submit", action="store_true", help="Create submission files.")
-    parser.add_option("--sample", action="store_true", help="Use sample train dataset.")
-    parser.add_option("--model", type="string", help="ML Model")
-    parser.add_option("--iter", type="string", help="Number of iterations (config option must not be provided.")
-    parser.add_option("--config", type="string", help="Configuration file for model (iter option must not be provided).")
-    kwargs, _ = parser.parse_args(args=None, values=None)
     logger = LogConf.create(logging)
-
-    check_options(kwargs)
-    action, param = determine_action(kwargs)
-    DATA = FilesConfig.Names.train_sample_data if getattr(kwargs, "sample") else FilesConfig.Names.train_data
-
-    main(getattr(kwargs, "model"), param, action, getattr(kwargs, "submit"))
+    cli = CLI(logger)
+    action, param = cli.determine_action()
+    DATA = FilesConfig.Names.train_sample_data if cli.get("sample") else FilesConfig.Names.train_data
+    main(cli.get("model"), param, action, cli.get("submit"))
 
---
+++ util/cli.py
@@ -0,0 +1,52 @@
+import os
+
+from optparse import OptionParser
+
+RANDOM_SEARCH = "random-search"
+MODEL_EVALUATION = "model-eval"
+
+
+class CLI(object):
+
+    def __init__(self, logger):
+        parser = OptionParser()
+        parser.add_option("--submit", action="store_true", help="Create submission files.")
+        parser.add_option("--sample", action="store_true", help="Use sample train dataset.")
+        parser.add_option("--model", type="string", help="ML Model")
+        parser.add_option("--iter", type="string", help="Number of iterations (config option must not be provided.")
+        parser.add_option("--config", type="string", help="Configuration file for model (iter option must not be provided).")
+        self.opts, _ = parser.parse_args(args=None, values=None)
+        self.logger = logger
+        self.check_options()
+
+    def check_options(self):
+        self.logger.info("[function call] check_options(opts=%s)" % str(self.opts))
+        model_opt = getattr(self.opts, "model")
+        iter_opt = getattr(self.opts, "iter")
+        config_opt = getattr(self.opts, "config")
+        valid_models = ["xgboost", "random-forest"]
+
+        if model_opt is None:
+            raise ValueError("A model must be provided.")
+
+        if model_opt not in valid_models:
+            raise ValueError("Invalid model: %s not in %s" % (model_opt, str(valid_models)))
+
+        if iter_opt:
+            int(iter_opt)
+
+        if config_opt:
+            if not os.path.exists(config_opt):
+                raise ValueError("File %s does not exists." % config_opt)
+
+        if sum([isinstance(e, str) for e in [iter_opt, config_opt]]) != 1:
+            raise ValueError("Exactly one flag must be used: either --iter or --config")
+
+    def determine_action(self):
+        config_opt = getattr(self.opts, "config")
+        if not config_opt:
+            return RANDOM_SEARCH, getattr(self.opts, "iter")
+        return MODEL_EVALUATION, config_opt
+
+    def get(self, key):
+        return getattr(self.opts, key)
