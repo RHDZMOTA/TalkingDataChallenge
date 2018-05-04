e15fc29d291ea935d953758ba0bcd58d2d570a65
rhdzmota <rohdzmota@gmail.com>
2018-05-04 15:01:02 -0500

    untested quickfix

--- util/spark.py
+++ util/spark.py
@@ -14,12 +14,22 @@ def collect_submit(output_dir, logger):
     files = [file for file in os.listdir(output_dir) if (".csv" in file) and (".crc" not in file)]
     csv_file = output_dir.replace("-folder", "")
 
-    df = pd.read_csv(output_dir + "/" + files[0])
-    df.to_csv(csv_file, index=None)
+    df = pd.read_csv(output_dir + "/" + files[0], header=None)
+    df.columns = ["click_id", "is_attributed"]
+    df.to_csv(csv_file, index=None, float_format="%0.8f")
     
     for file in files[1:]:
         df = pd.read_csv(output_dir + "/" + file)
         with open(csv_file, "a") as f:
-            df.to_csv(f, index=None, header=False)
+            df.to_csv(f, index=None, header=None, float_format="%0.8f")
     shutil.rmtree(output_dir)
-    return df
+    del df
+    
+    # TODO: find bug
+    from conf.settings import FilesConfig
+    df = pd.read_csv(csv_file)
+    submit_data = pd.read_csv(FilesConfig.submit_data)[["click_id"]]
+    temp = pd.merge(submit_data, df, on="click_id", how="left")
+    del df, submit_data
+    temp.to_csv(csv_file, index=None, float_fomat="%0.8f")
+    
